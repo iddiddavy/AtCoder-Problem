f0665509c5476c1eb9ddbb222ec26e314d12b4ad
iddiddavy <86935409+iddiddavy@users.noreply.github.com>
2023-06-24 07:52:23 +0700

    AtCoder_Beginner_Contest_306_A_Echo

---
+++ abc306_a.cpp
@@ -0,0 +1,13 @@
+//Link: https://atcoder.jp/contests/abc306/tasks/abc306_a
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	for (int i = 0; i < n; i++)
+		cout << s[i] << s[i];
+	cout << endl;
+}
